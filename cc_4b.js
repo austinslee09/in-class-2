20d4d64cee694e37582db10741adbaad6e2515d8
Austin Lee <austinlee@usf.edu>
2025-09-17 17:56:17 -0400

    spelling?

--- cc_4b.js
+++ cc_4b.js
@@ -1,9 +1,9 @@
 let products=[
-    {name:"brick", sku:"sku-981", Price: .99,Inventory:"90",catagory:"materials"},
-    {name:"cat", sku:"sku-872", Price:6, Inventory:"25",catagory:"pet"},                       
-    {name:"table", sku:"sku-132", Price:8, Inventory:"40", catagory:"furnature"},
-    {name:"wonton", sku:"sku-901", Price:16, Inventory:"70", catagory:"food"},
-    {name:"seeds", sku:"sku-343", Price:4, Inventory:"100",catagory:"farming"}
+    {name:"brick", sku:"sku-981", price: .99,Inventory:"90",catagory:"materials"},
+    {name:"cat", sku:"sku-872", price:6, Inventory:"25",catagory:"pet"},                       
+    {name:"table", sku:"sku-132", price:8, Inventory:"40", catagory:"furnature"},
+    {name:"wonton", sku:"sku-901", price:16, Inventory:"70", catagory:"food"},
+    {name:"seeds", sku:"sku-343", price:4, Inventory:"100",catagory:"farming"}
 ]
 console.log(products);
 
